955b4607675bcf205a1c5297bd02bcf52469973b
Middeon <34604120+Middeon@users.noreply.github.com>
2017-12-17 11:00:45 -0500

    Add files via upload

---
+++ Learning/TwelveDaysofChristmas.java
@@ -0,0 +1,50 @@
+package songs;
+
+public class TwelveDaysofChristmas {
+
+	public static void main(String[] args) {
+		int day=1;
+		string order;
+			if (day==1);
+			while (day<13){
+							order = "On the first";		
+							if (day==2){
+								order = "On the second";}
+							if (day==3);
+							order = "On the third";
+							if (day==4);
+							order = "On the fourth";
+							if (day==5);
+							order = "On the fith";
+							if (day==6);
+							order = "On the sixth";
+							if (day==7);
+							order = "On the seventh";
+							if (day==8);
+							order = "On the eighth";
+							if (day==9);
+							order = "On the nineth";
+							if (day==10);
+							order = "On the tenth";
+							if (day==11);
+							order = "On the eleventh";
+							if (day==12);
+							order = "On the twelfth";
+							day++;
+			}
+
+			if (day==1);
+			String gift = "A partridge in a pear tree.";
+			if (day==2);
+			String gift = "Two turtle doves";
+			if (day==3);
+			String gift = "Three french hens";
+			if (day==3);
+			
+		
+		
+	 refrain = "day of Christmas my true love sent to me";
+	 System.out.println(order + refrain);
+		;}
+			if (day==1)
+}
